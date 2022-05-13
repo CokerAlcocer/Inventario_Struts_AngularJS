6702fd7be73fc49b309e4725ab1bf30e98a61036
CokerAlcocer <20203tn066@utez.edu.mx>
2022-05-13 15:40:47 -0500

    UPDATE 13/05/2022

---
+++ database/inventory_script.sql
@@ -0,0 +1,72 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema inventory
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema inventory
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `inventory` DEFAULT CHARACTER SET utf8 ;
+USE `inventory` ;
+
+-- -----------------------------------------------------
+-- Table `inventory`.`category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `inventory`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `inventory`.`brand`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `inventory`.`brand` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `image_url` TEXT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `inventory`.`product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `inventory`.`product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `quantity` BIGINT NOT NULL DEFAULT 0,
+  `description` TEXT NULL,
+  `image_url` TEXT NOT NULL,
+  `brand_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nombre_UNIQUE` (`name` ASC) VISIBLE,
+  INDEX `fk_product_brand_idx` (`brand_id` ASC) VISIBLE,
+  INDEX `fk_product_category1_idx` (`category_id` ASC) VISIBLE,
+  CONSTRAINT `fk_product_brand`
+    FOREIGN KEY (`brand_id`)
+    REFERENCES `inventory`.`brand` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_product_category1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `inventory`.`category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
